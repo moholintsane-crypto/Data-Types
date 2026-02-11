05250dd6d72fc01f355b8407ab3588a86f2a0955
moholintsane-crypto <moholine1@gmail.com>
2026-02-11 18:25:32 +0200

    initial commit

---
+++ Data Types.py
@@ -0,0 +1,12 @@
+age=20
+weight=40.5
+is_student=True
+name="Ntsane"
+print(type(age))
+print(type(weight))
+print(type(is_student))
+print(type(name))
+
+#typecasting
+weight=int(weight)
+print("the new data type is", type(weight))
---
+++ Typecasting.py
@@ -0,0 +1,13 @@
+name=input("Enter your name")
+age=int(input("Enter your age"))
+print(name)
+print(age)
+
+school="Codingal"
+institution="Education"
+print(school.upper())
+#concatenation is adding two strings together
+conc=school+institution
+print(conc)
+length=len(conc)
+print("the length of the word is", length)
